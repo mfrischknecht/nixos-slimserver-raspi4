30abab3c82ff8246ecdfcf2b378d76b334c0368d
Manuel Frischknecht <manuel.frischknecht@gmail.com>
2021-01-19 19:59:22 +0000

    Add a basic Raspberry Pi 4 config

---
+++ configuration.nix
@@ -0,0 +1,30 @@
+{ config, pkgs, lib, ... }:
+{
+  services.sshd.enable = true;
+  networking.hostName = "nixos";
+
+  environment.systemPackages = with pkgs; [ vim git tmux htop ];
+
+  users.users.nixos = {
+    isNormalUser = true;
+    home = "/home/nixos";
+    extraGroups = [ "wheel" ];
+  };
+
+  nixpkgs.config.allowUnfree = true;
+  boot.loader.grub.enable = false;
+  boot.loader.raspberryPi.enable = true;
+  boot.loader.raspberryPi.version = 4;
+  boot.kernelPackages = pkgs.linuxPackages_rpi4;
+  boot.kernelParams = ["cma=256M"];
+  hardware.enableRedistributableFirmware = true;
+
+  fileSystems = {
+    "/" = {
+      device = "/dev/disk/by-label/NIXOS_SD";
+      fsType = "ext4";
+    };
+  };
+
+  swapDevices = [ { device = "/swapfile"; size = 1024; } ];
+}
